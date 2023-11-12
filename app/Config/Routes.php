1c5c080e4ddc25e9742a45adbd5b75c9bdcb414e
guntureka <guntureka1020@gmail.com>
2023-11-13 04:54:21 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+<<<<<<< Updated upstream
 $routes->get('/login', 'page::loginForm');
 $routes->get('/register', 'page::registerForm');
+=======
+$routes->resource('user');
+$routes->post('/user/(:num)', 'User::updatePassword/$1');
+$routes->resource('game');
+$routes->resource('topup');
+$routes->resource('transaction');
+>>>>>>> Stashed changes
